8aa3350fd3adc28259d70b042211503ae69c72ee
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-29 17:38:06 -0300

    Auth0 properly configured and Log in test button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,20 @@ import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module'; // Autoimport Línea 11 - Modulo custom: HomeModule
 import { LoginModule } from './modules/login/login.module'; // Autoimport Línea 11 - Modulo custom: LoginModule
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule, HomeModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule,
+    HomeModule,
+    AuthModule.forRoot({
+      domain: 'rmouzoapp.us.auth0.com',
+      clientId: 'heLoPGZ11OwdehoepLVuIiFg3hJWs0iS',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
+import { AuthButtonComponent } from './components/auth-button/auth-button.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, AuthButtonComponent],
   imports: [CommonModule],
 })
 export class LoginModule {}
