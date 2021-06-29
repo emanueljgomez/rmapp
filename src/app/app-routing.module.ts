6d4f38e9d6ef46cb5809b1ac3f27a23289c64561
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-29 15:22:37 -0300

    Basic routing for LoginComponent established. Bootstrap 5 installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/components/login/login.component'; // Autoimport Líne 5 -- Path a LoginComponent definido, se renderizará en el router-outlet
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
